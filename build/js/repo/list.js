c8a310d21d9b4630fac051c1c89a4f5b8273a06a
Jake Smith <jakehockey10@gmail.com>
2015-02-21 15:55:29 -0700

    build

--- build/js/repo/list.js
+++ build/js/repo/list.js
@@ -1,23 +1,23 @@
 // Q: How is this js file loaded?
 function repoList(){
 
-    console.log('listing repos')    
+    console.log('listing repos');
     
     $.get("https://api.github.com/orgs/ucdd2-sp15/repos", github, function(data) {
 
         // Q: What is the parameter 'github'? Where was it defined? What's the purpose? 
 
         // Q: Why is JSON.parse no longer necessary?
-        var repos = data
+        var repos = data;
         
         // Q. Why are these templates files stored in a separate folder inside contents/?
         $.get("/git-jquery/templates/repoList.jade", function(template) {
 
             // render the template
-            var html = jade.render(template, {items: repos})            
+            var html = jade.render(template, {items: repos, title: 'Repos'});
 
             // assign the rendered html to the dom element whose id is #list
-            $("#list").html(html)
+            $("#list").html(html);
 
             // load the first repo to view
             repoView(repos[0].full_name)
--- contents/js/my/events.js
+++ contents/js/my/events.js
@@ -3,5 +3,27 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    alert('todo')
+    console.log('listing events');
+
+    $.get("https://api.github.com/users/jakehockey10/events", github, function(data) {
+
+        // Q: What is the parameter 'github'? Where was it defined? What's the purpose?
+
+        // Q: Why is JSON.parse no longer necessary?
+        var events = data;
+
+        // Q. Why are these templates files stored in a separate folder inside contents/?
+        $.get("/git-jquery/templates/eventList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: events});
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html);
+
+            // clear out details if we looked at repos prior to viewing events list.
+            $('#details').html('');
+        })
+
+    })
 }
